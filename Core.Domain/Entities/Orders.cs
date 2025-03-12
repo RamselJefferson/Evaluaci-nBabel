29bae7300da75780075ff18cc1e3f5fed67672f8
Jefferson De La Cruz <ramseldelacruz@outlook.es>
2025-03-12 18:41:21 -0400

    Agregar archivos de proyecto.

---
+++ Core.Domain/Entities/Orders.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Domain.Entities
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+    }
+}
